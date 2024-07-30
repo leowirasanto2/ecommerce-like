1a797826668f59b6adafd6d6b57c1c56fdcd9db6
Leo Wirasanto Laia <leowirasanto@gmail.com>
2024-07-30 15:03:49 +0800

    init xcode project

---
+++ ECommerceLike/ECommerceLike/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ECommerceLike
+//
+//  Created by Leo Wirasanto Laia on 30/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ECommerceLike/ECommerceLike/ECommerceLikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceLikeApp.swift
+//  ECommerceLike
+//
+//  Created by Leo Wirasanto Laia on 30/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceLikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
