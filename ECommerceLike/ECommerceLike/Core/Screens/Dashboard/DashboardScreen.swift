8edcfc74f268b30f4f441914c6855490559632ac
Leo Wirasanto Laia <leowirasanto@gmail.com>
2024-07-30 15:06:52 +0800

    create files for wireframe

---
+++ ECommerceLike/ECommerceLike/Core/Screens/Dashboard/DashboardScreen.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardScreen.swift
+//  ECommerceLike
+//
+//  Created by Leo Wirasanto Laia on 30/07/24.
+//
+
+import SwiftUI
+
+struct DashboardScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardScreen()
+}
---
+++ ECommerceLike/ECommerceLike/Core/Screens/ProductDetail/ProductDetailScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailScreen.swift
+//  ECommerceLike
+//
+//  Created by Leo Wirasanto Laia on 30/07/24.
+//
+
+import SwiftUI
+
+struct ProductDetailScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailScreen()
+}
